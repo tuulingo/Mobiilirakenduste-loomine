c740ad6909beaf794fb8e4ef7a186dbf6edfdb18
tuulingo <oliver.tuuling@tptlive.ee>
2019-10-31 09:52:23 +0200

    StarWarsApp
    Search & Icons

---
+++ StarWarsApp.Core/DataService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using StarWarsApp.Core.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StarWarsApp.Core
+{
+    public class DataService
+    {
+        public static async Task<People> GetStarWarsPeople(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(queryString);
+
+            People data = null;
+            if (response != null)
+            {
+                data = JsonConvert.DeserializeObject<People>(response);
+            }
+            return data;
+        }
+    }
+}
---
+++ StarWarsApp/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using StarWarsApp.Core;
+using static StarWarsApp.PeopleAdapter;
+
+namespace StarWarsApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            var searchField = FindViewById<EditText>(Resource.Id.searchBarEditText);
+            var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+            var peopleListView = FindViewById<ListView>(Resource.Id.peopleDataListView);
+
+            searchButton.Click += async delegate
+            {
+                var searchText = searchField.Text;
+                var queryString = "https://swapi.co/api/people/?search=" + searchText;
+                var data = await DataService.GetStarWarsPeople(queryString);
+                peopleListView.Adapter = new StarWarsPeopleAdapter(this, data.Results);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
