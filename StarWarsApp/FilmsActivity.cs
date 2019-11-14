823aba52c9ea37787948e3d2c3775b0f02c22e3c
tuulingo <oliver.tuuling@tptlive.ee>
2019-11-14 09:48:26 +0200

    Get item position

--- StarWarsApp/FilmsActivity.cs
+++ StarWarsApp/FilmsActivity.cs
@@ -10,6 +10,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using StarWarsApp.Core;
+using static Android.Widget.AdapterView;
 using static StarWarsApp.FilmsAdapter;
 
 namespace StarWarsApp
@@ -32,6 +33,18 @@ namespace StarWarsApp
                 var queryString = "https://swapi.co/api/films/?search=" + searchText;
                 var data = await DataService.GetStarWarsFilms(queryString);
                 listView.Adapter = new StarWarsFilmsAdapter(this, data.Results);
+            };
+
+            listView.ItemClick += (object sender, ItemClickEventArgs e) =>
+            {
+                var a = Convert.ToString(listView.GetItemIdAtPosition(e.Position));
+                var b = Convert.ToString(e.Position);
+
+                Context context = Application.Context;
+
+                ToastLength duration = ToastLength.Short;
+                var toast = Toast.MakeText(context , b, duration);
+                toast.Show();
             };
 
         }
