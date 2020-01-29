f71c4a1fccc51123ee706768811051f463609ece
oliver tuuling <oliver.tuuling@tptlive.ee>
2020-01-30 00:22:19 +0200

    Open local file

--- FirstForms/FirstForms/FirstForms/MainPage.xaml.cs
+++ FirstForms/FirstForms/FirstForms/MainPage.xaml.cs
@@ -15,49 +15,60 @@ namespace FirstForms
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
-    {       
-        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "lambi.txt");
+    {
+        string _localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string _filePath = "";
+        string[] _files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+        Dictionary<string, string> filesDict = new Dictionary<string, string>();
 
         public MainPage()
         {
             InitializeComponent();
-            if (File.Exists(_fileName))
+            if (File.Exists(_filePath))
             {
-                Editor.Text = File.ReadAllText(_fileName);
+                Editor.Text = File.ReadAllText(_filePath);
             }
         }
 
         int count = 0;
         void OnSaveButtonClicked(object sender, EventArgs e)
         {
-            File.WriteAllText(_fileName, Editor.Text);
+            File.WriteAllText(_filePath, Editor.Text);
         }
 
         void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-            if (File.Exists(_fileName))
+            if (File.Exists(_filePath))
             {
-                File.Delete(_fileName);
+                File.Delete(_filePath);
             }
             Editor.Text = string.Empty;
         }
 
         void OnOpenClick(object sender, EventArgs e)
         {
-            if (File.Exists(_fileName))
+            if (FileName.Text != "")
             {
-                var
+                _filePath = Path.Combine(_localFolder, FileName.Text + ".txt");
+            }
+
+            if (File.Exists(_filePath))
+            {
+                Editor.Text = File.ReadAllText(_filePath);
             }
             else
             {
-                FileName.Text = "File doesn't exist";
+                Editor.Text = "File doesn't exist.";
             }
         }
-
-        public string GetFilePath()
+        public void GetFiles(string[] filePaths)
         {
-            Context context = AppContext;
-            var filepath = context.get
+            foreach (string path in filePaths)
+            {
+                string[] currFilePathParts = path.Split(new[] { "\\" }, StringSplitOptions.None);
+                string currFileName = currFilePathParts[currFilePathParts.Length - 1];
+                filesDict.Add(currFileName, path);
+            }
         }
     }
 }
