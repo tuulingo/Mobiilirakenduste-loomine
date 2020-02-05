a36c3558a11bc276126cd64918c68207f74b800e
oliver tuuling <oliver.tuuling@tptlive.ee>
2020-02-06 00:37:20 +0200

    Katki pooleli register

---
+++ Register/Register_Login/Register_Login/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Register_Login
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        UserDB userData;
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this.mainPage, true);
+            userData = new UserDB();
+            userNameEntry.ReturnCommand = new Command(() => passwordEntry.Focus());
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            if (userNameEntry.Text != null && passwordEntry.Text != null)
+            {
+                var validData = userData.LoginValidate(userNameEntry.Text, passwordEntry.Text);
+                if (validData)
+                {
+                    popupLoadingView.IsVisible = false;
+                }
+                else
+                {
+                    popupLoadingView.IsVisible = false;
+                    await DisplayAlert("Login Failed", "Username or Password Incorrect", "OK");
+                }
+            }
+            else
+            {
+                popupLoadingView.IsVisible = false;
+                await DisplayAlert("He He", "Enter User Name and Password Please", "OK");
+            }
+        }
+
+        private async void SignUp_Clicked(object sender, EventArgs e)
+        { 
+
+        }
+        
+
+       
+    }
+}
