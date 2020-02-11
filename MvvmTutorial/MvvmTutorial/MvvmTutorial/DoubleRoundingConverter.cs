0891706a542b813255af9a7083092f99573d8d4e
roots79 <opilane@tptlive.ee>
2020-02-11 09:51:35 +0200

    Testid, mis mul kaotsi läksid

---
+++ MvvmTutorial/MvvmTutorial/MvvmTutorial/DoubleRoundingConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MvvmTutorial
+{
+    public class DoubleRoundingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Round((double)value, parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Round((double)value, parameter);
+        }
+
+        double Round (double number, object parameter)
+        {
+            double precision = 0.1;
+            if (parameter != null)
+            {
+                precision = double.Parse((string)parameter);
+            }
+            return precision * Math.Round(number / precision);
+        }
+    }
+}
---
+++ MvvmTutorial/MvvmTutorial/MvvmTutorial/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MvvmTutorial.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MainViewModel()
+        {
+            _subTotal = 120;
+            _generosity = 0.1;
+
+            Recalculate();
+        }
+
+        private double _tip;
+        public double Tip
+        {
+            get { return _tip; }
+            set
+            {
+                if (_tip != value)
+                {
+                    _tip = value;
+                    OnPropertyChanged(nameof(Tip));
+                }
+            }
+        }
+
+        private double _subTotal;
+        public double SubTotal
+        {
+            get { return _subTotal; }
+            set
+            {
+                if (_subTotal != value)
+                {
+                    _subTotal = value;
+                    OnPropertyChanged(nameof(SubTotal));
+                    Recalculate();
+                }
+            }
+        }
+
+        private double _generosity;
+        public double Generosity
+        {
+            get { return _generosity; }
+            set
+            {
+                if (_generosity != value)
+                {
+                    _generosity = value;
+                    OnPropertyChanged(nameof(Generosity));
+                    Recalculate();
+                }
+            }
+        }
+
+        private double _total;
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                if (_total != value)
+                {
+                    _total = value;
+                    OnPropertyChanged(nameof(Total));
+                    Recalculate();
+                }
+            }
+        }
+
+
+        private void Recalculate()
+        {
+            Math.Round(Tip = SubTotal * Generosity);
+            Total = Tip + SubTotal;
+        }
+    }
+}
