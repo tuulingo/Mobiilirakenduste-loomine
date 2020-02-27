79cab25e6e1c819cafb32d416b35d2b762e1e1a0
tuulingo <oliver.tuuling@tptlive.ee>
2020-02-27 09:53:43 +0200

    Take a picture (no save)

--- MvvmTutorial/MvvmTutorial/MvvmTutorial.Android/MainActivity.cs
+++ MvvmTutorial/MvvmTutorial/MvvmTutorial.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace MvvmTutorial.Droid
 {
@@ -22,12 +23,17 @@ namespace MvvmTutorial.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
         }
     }
 }
--- MvvmTutorial/MvvmTutorial/MvvmTutorial/PicturesPage.xaml.cs
+++ MvvmTutorial/MvvmTutorial/MvvmTutorial/PicturesPage.xaml.cs
@@ -15,6 +15,26 @@ namespace MvvmTutorial
         public PicturesPage()
         {
             InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
         }
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        //https://www.youtube.com/watch?v=rxMx-WSZrTc
+
     }
 }
